29d0108be56d371f74ec4314dac7cec23920e99a
kattelbiplov <kattelbiplov5@gmail.com>
2024-10-13 12:21:26 +0545

    finally

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5500;
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8081'], // Allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-    credentials: true // Include credentials (cookies, authorization headers, etc.)
-}));
-app.options('*', cors());
+
+// Allow all origins
+app.use(cors()); // This will allow all origins
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form data
 app.use(cookieParser());
 
 const dbConfig = require('./config/dbConfig');
 const userRoute = require('./routes/userRoutes');
-const transactionRoute=require('./routes/transactionRoutes');
-const customerServiceRoute=require('./routes/customerServiceRoutes');
-app.use('/api/users', userRoute); 
-app.use('/api/users',transactionRoute)
-app.use('/api/users',customerServiceRoute)
+const transactionRoute = require('./routes/transactionRoutes');
+const customerServiceRoute = require('./routes/customerServiceRoutes');
+
+app.use('/api/users', userRoute);
+app.use('/api/users', transactionRoute);
+app.use('/api/users', customerServiceRoute);
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
