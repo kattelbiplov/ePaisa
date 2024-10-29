8fbd124169a3c79df52223a91492c76b734fc15f
kattelbiplov <kattelbiplov5@gmail.com>
2024-10-29 23:34:46 +0545

    first commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 mongoose.connect(process.env.mongo_url);
 
 const connectionResult = mongoose.connection;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5500;
 
